560db86a388935ffc3f8b68f5931720dcac50d70
SaiTarunA <saitarunavadhootha@gmail.com>
2022-11-03 12:44:39 +0530

    Added Notification

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Time from './components/Time';
 import BirthdaysInput from './components/BirthdaysInput'
 import BirthdayList from './components/BirthdayList';
 import Login from './components/Login';
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes, useNavigate} from 'react-router-dom'
 import NavBar from './components/NavBar';
 import Divider from '@mui/material/Divider';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -13,6 +13,9 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import BirthdayDashboard from './components/BirthdayDashboard';
+import { auth, db } from './firebase';
+import { onValue, ref } from 'firebase/database';
+import logo from "./public/logo192.png";
 
 const style = {
   position: 'absolute',
@@ -49,6 +52,107 @@ function App() {
     }
   }, [networkStatus])
   
+  const [granted, setGranted] = React.useState(false)
+  const [allBirthdays, setallBirthdays] = React.useState([]);
+
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    let isCancelled = false;
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        onValue(ref(db, `/${auth.currentUser.uid}/BirthdayList`), (snapshot) => {
+          if (!isCancelled) {
+          setallBirthdays([]);
+          const data = snapshot.val();
+          if (data !== null) {
+            let localAllBirthdays = []
+            localAllBirthdays = Object.values(data).map((birthdaylist) => {
+              return birthdaylist
+            });
+            setallBirthdays(localAllBirthdays)
+          }
+        }
+        });
+      } else if (!user) {
+        navigate("/");
+      }
+    });
+    return () => {
+      isCancelled = true;
+      setallBirthdays([]);
+    };
+  }, []);
+
+  React.useEffect(() => {
+      if (Notification.permission === 'granted') {
+          setGranted(true)
+      } else if (Notification.permission !== 'denied') {
+          Notification.requestPermission().then(perm => {
+              setGranted(perm === 'granted' ? true : false)
+          })
+      }
+  }, [])
+
+  React.useEffect(() => {
+    let notification
+    let interval
+
+    document.addEventListener("visibilitychange", () => {
+        if (document.visibilityState === "hidden") {
+            let date = new Date()
+            let dateNumber = date.getDate()
+            let monthId = date.getMonth() + 1
+            let hours = date.getHours()
+            if (hours === 0) {
+              let requiredNames = []
+                  if (allBirthdays.length !== 0) {
+                    requiredNames =  allBirthdays
+                      .filter((p) => p.monthId == monthId && p.day == dateNumber)
+                      .map((p) => {
+                        return `${p.name}`;
+                      })
+                  }
+              notification = new Notification(`Today's Birthdays(${dateNumber}-${monthId})`, {
+                body: `${requiredNames.length !== 0 ? requiredNames : "No Birthdays"}`,
+                icon: logo,
+                tag: "Same Time"
+              })
+
+              notification.addEventListener('click', function(){
+                  window.open('https://saitaruna.github.io/birthdays/');
+              });
+            }
+            interval = setInterval(() => {
+                date = new Date()
+                dateNumber = date.getDate()
+                monthId = date.getMonth() + 1
+                hours = date.getHours()
+                if (hours === 0) {
+                  let requiredNames = []
+                  if (allBirthdays.length !== 0) {
+                    requiredNames =  allBirthdays
+                      .filter((p) => p.monthId == monthId && p.day == dateNumber)
+                      .map((p) => {
+                        return `${p.name}`;
+                      })
+                  }
+                  notification = new Notification(`Today's Birthdays(${dateNumber}-${monthId})`, {
+                    body: `${requiredNames.length !== 0 ? requiredNames : "No Birthdays"}`,
+                    icon: logo,
+                    tag: "Next Interval"
+                  })
+
+                  notification.addEventListener('click', function(){
+                      window.open('https://saitaruna.github.io/birthdays/');
+                  });
+                }
+            }, 3600000);
+        } else {
+            if (interval) clearInterval(interval)
+            if (notification) notification.close()
+        }
+    })
+  }, [allBirthdays])
 
   return (
     <div className="App">
